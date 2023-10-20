94b47f572f2ed96bbf0c2e5b6167e6d21e268a7c
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-10-19 23:11:27 -0400

    Bugfix typos

--- ml_model/utils/logger.py
+++ ml_model/utils/logger.py
@@ -5,14 +5,14 @@ from typing import Optional
 
 
 def get_logger(logger_name: str, save_path: Optional[Path] = None, backups: int = 5):
-    log = logging.get_logger(logger_name)
-    if len(log, handlers) == 0:
+    log = logging.getLogger(logger_name)
+    if len(log.handlers) == 0:
         log.setLevel(logging.INFO)
         format = logging.Formatter(
             "[%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s() - %(levelname)s %(message)s"
         )
 
-        ch = logging.streamhandler()
+        ch = logging.StreamHandler()
         ch.setFormatter(format)
         log.addHandler(ch)
 
