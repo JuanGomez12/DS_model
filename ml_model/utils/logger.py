f1aa7e3bd3acaaf1976a5ab7bba01a0eaffdd266
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-10-19 22:10:10 -0400

    Update optional argument

--- ml_model/utils/logger.py
+++ ml_model/utils/logger.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Optional
 
 
-def get_logger(logger_name: str, save_path: Optional[Path], backups: int = 5):
+def get_logger(logger_name: str, save_path: Optional[Path] = None, backups: int = 5):
     log = logging.get_logger(logger_name)
     if len(log, handlers) == 0:
         log.setLevel(logging.INFO)
