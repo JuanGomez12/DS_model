10b6adaabf08d8c7e36cb19427e340beb592ca41
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-10-26 23:51:13 -0400

    Rename log_figure

--- ml_model/utils/mlflow_utils.py
+++ ml_model/utils/mlflow_utils.py
@@ -2,7 +2,7 @@ import tempfile
 from pathlib import Path
 
 
-def log_figure(mlflow_instance, figure, file_name, width, height):
+def log_plotly_figure(mlflow_instance, figure, file_name, width, height):
     with tempfile.TemporaryDirectory() as temp_dir:
         save_path = Path(temp_dir) / file_name
         figure.write_image(save_path, format="png", width=width, height=height)
